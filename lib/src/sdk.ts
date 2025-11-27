64dbbc66d13c235d2bd9255bd4c3d674de3a91ba
CL TheDreWen <louis.charneux@gmail.com>
2025-11-27 15:25:08 +0100

    Add getInfoFromExternalServer method and update ServerInfo type in ScriptSDK

--- lib/src/sdk.ts
+++ lib/src/sdk.ts
@@ -40,8 +40,18 @@ class ScriptSDK {
         const regex = new RegExp(`^([a-z]*);#;(\\d{3});#;${repeatedPattern}$`, 'gm');
         return regex.exec(message);
     }
+
+    private getResultBody(result: string[], length: number) : string[] {
+        const cleanResult = new Array(length);
+        result.map((value, index) => {
+            if(index > 2) {
+                cleanResult[index - 3] = value;
+            }
+        });
+        return cleanResult;
+    }
     
-    public async send(action: string, body: string[], args: number = 1): Promise<{ success: boolean, code: number, result: string }> {
+    public async send(action: string, body: string[], args: number = 1): Promise<{ success: boolean, code: number, result: string[] }> {
         return new Promise((resolve) => {
             
             const id = this.generateId();
@@ -54,7 +64,7 @@ class ScriptSDK {
                     resolve({
                         success: m[1] == 'true',
                         code: parseInt(m[2]),
-                        result: m[3]
+                        result: m[1] == 'true' ? this.getResultBody(m, args) : [m[3]]
                     });
                 }
             }
